4dbc93cfe285c5ce35d8e3efe9b643cbf954809b
Sergey Balashov <Balashov.152@gmail.com>
2023-02-08 22:43:48 +0300

    IOS-2305 Use new now node config

--- BlockchainSdk/Common/WalletManagerFactory.swift
+++ BlockchainSdk/Common/WalletManagerFactory.swift
@@ -463,7 +463,7 @@ public class WalletManagerFactory {
             $0.networkService = BitcoinNetworkService(
                 providers: [
                     BlockBookUtxoProvider(blockchain: .ravencoin(testnet: testnet),
-                                          blockBookConfig: NowNodesBlockBookConfig(apiKey: config.nowNodesApiKey),
+                                          blockBookConfig: NowNodesBlockConfig(apiKey: config.nowNodesApiKey),
                                           networkConfiguration: networkProviderConfiguration)
                     .eraseToAnyBitcoinNetworkProvider()
                 ]
